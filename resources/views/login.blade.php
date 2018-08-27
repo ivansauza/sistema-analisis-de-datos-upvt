5b51b05c2bad1bbf16ba04a10eaa46e29fd2e9fb
Ivan Sauza <sauuza@gmail.com>
2018-08-26 20:13:42 -0500

    Se aniadio bootstrap4, jquery, popper.js. Se modifico el archivo de rutas para que aparesca en la raiz del sistema el login.

---
+++ resources/views/login.blade.php
@@ -0,0 +1,23 @@
+<!doctype html>
+<html lang="{{ app()->getLocale() }}">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+
+        <title>Laravel</title>
+
+        <!-- Fonts -->
+        
+
+        <!-- Styles -->
+        <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
+    </head>
+
+    <body>
+
+        <script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
+        <script type="text/javascript" src="{{ asset('js/popper.min.js') }}"></script>
+    </body>
+</html>
