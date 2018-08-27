e32b0ce63fdeca428338679cd6b9dab9d65947c4
Ivan Sauza <sauuza@gmail.com>
2018-08-26 19:31:00 -0500

    Se corrigieron errores en los nombres de las clases

--- database/migrations/2018_08_26_234730_create_preguntas_table.php
+++ database/migrations/2018_08_26_234730_create_preguntas_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateInformacionesTable extends Migration
+class CreatePreguntasTable extends Migration
 {
     /**
      * Run the migrations.
--- database/migrations/2018_08_27_001352_create_encuesta_pregunta_table.php
+++ database/migrations/2018_08_27_001352_create_encuesta_pregunta_table.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class CreateEncuestaInformacionTable extends Migration
+class CreateEncuestaPreguntaTable extends Migration
 {
     /**
      * Run the migrations.
