f466243309ed42ed3a8ca875f22634fbb41d7e8f
Ivan Sauza <sauuza@gmail.com>
2018-08-27 21:51:10 -0500

    Se aniadio el controlado, modelo, ruta de la entidad programa

---
+++ app/Http/Controllers/ProgramaController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class ProgramaController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+
+    /*public function __construct()
+    {
+        $this->middleware('auth');
+    }*/
+
+    public function index()
+    {
+        return view('programa/index');
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('programa/create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return view('programa/show');
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        return view('programa/edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -15,6 +15,27 @@
     </head>
 
     <body>
+		<div class="container-fluid mt-5">
+			<div class="row">
+				<div class="col-md-4 col-sm-6 col-xs-12 mx-auto">
+					<h4 class="text-center">Porfavor, inicia sesión</h4>
+
+					<form method="POST" action="" class="mt-5">
+						<div class="form-group">
+							<input type="email" name="email" id="email" class="form-control" placeholder="Correo electrónico">
+						</div>
+
+						<div class="form-group">
+							<input type="password" name="password" id="password" class="form-control" placeholder="Contraseña">
+						</div>
+
+						<div class="form-group mt-5">
+							<input type="submit" name="Entrar" class="btn btn-outline-primary btn-block">
+						</div>
+					</form>
+				</div>
+			</div>
+		</div>
 
         <script type="text/javascript" src="{{ asset('js/jquery-3.3.1.min.js') }}"></script>
         <script type="text/javascript" src="{{ asset('js/bootstrap.min.js') }}"></script>
