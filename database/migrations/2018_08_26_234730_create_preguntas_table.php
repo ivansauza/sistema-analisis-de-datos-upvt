b5daa01695717d15ab3a2b717fcdf4714db4f991
Ivan Sauza <sauuza@gmail.com>
2018-08-29 14:50:45 -0500

    Se agrego la nueva tabla pivote de pregunta_programa

--- database/migrations/2018_08_26_234730_create_preguntas_table.php
+++ database/migrations/2018_08_26_234730_create_preguntas_table.php
@@ -16,8 +16,8 @@ class CreatePreguntasTable extends Migration
         Schema::create('preguntas', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre')->unique();
-            $table->integer('programa_id')->unsigned();
-            $table->foreign('programa_id')->references('id')->on('programas');
+            /*$table->integer('programa_id')->unsigned();
+            $table->foreign('programa_id')->references('id')->on('programas');*/
             /*$table->integer('role_id')->unsigned();
             $table->foreign('role_id')->references('id')->on('roles');*/
             $table->timestamps();
