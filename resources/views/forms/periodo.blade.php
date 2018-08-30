88b6473ff607f7fd59c2f4a8f8dd674183645c42
Ivan Sauza <sauuza@gmail.com>
2018-08-29 21:40:41 -0500

    Se agregaron los inputs respectivos al formulario del modulo periodos

---
+++ resources/views/forms/periodo.blade.php
@@ -0,0 +1,18 @@
+<div class="form-row">
+	<div class="form-group col">
+		{{ Form::label('periodo') }}
+		{{ Form::select('periodo', ['a' => 'Enero-Abril', 'b' => 'Mayo-Agosto', 'c' => 'Septiembre-Diciembre'],null, ['class' => 'form-control']) }}
+	</div>
+
+	<div class="form-group col-6">
+		{{ Form::label('anio', 'Año') }}
+		{{ Form::number('anio', null, ['class' => 'form-control', 'min' => 1990, 'max' => date("Y")]) }}
+	</div>
+</div>
+
+<hr />
+
+<div class="custom-control custom-checkbox">
+ 	{{ Form::checkbox('estado', 'null', false, ['class' => 'custom-control-input', 'id' => 'estado']) }}
+ 	{{ Form::label('estado', 'Activar el periodo, el usuario podra ver los datos a este periodo.', ['class' => 'custom-control-label']) }}
+</div>
