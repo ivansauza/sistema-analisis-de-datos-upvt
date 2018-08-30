feec75046586c49a343e99bb7853b0e208856c67
Ivan Sauza <sauuza@gmail.com>
2018-08-29 21:51:00 -0500

    Se agregaron los inputs respectivos al formulario del modulo preguntas

--- resources/views/forms/pregunta.blade.php
+++ resources/views/forms/pregunta.blade.php
@@ -2,3 +2,22 @@
 	{{ Form::label('nombre') }}
 	{{ Form::text('nombre', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa la pregunta']) }}
 </div>
+
+<div class="form-group">
+	{{ Form::label('rol_id', 'Rol') }}
+	{{ Form::select('rol_id', ['a' => 'Maestro', 'b' => 'Secretaria', 'c' => 'Director'], null, ['class' => 'form-control']) }}
+	<small class="form-text text-muted">Seleccione el Rol de Usuario que podra llenar la información a esta pregunta.</small>
+</div>
+
+<div class="form-group">
+	{{ Form::label('programa_id', 'Programa Educativo') }}
+	{{ Form::select('programa_id', ['a' => 'Ingenieria en Informatica', 'b' => 'Maestria en Administracion', 'c' => 'Ingenieria Inddustrial'], null, ['class' => 'form-control', 'multiple' => true]) }}
+	<small class="form-text text-muted">Puede seleccionar varios programas presionando la tecla "control" + "click".</small>
+</div>
+
+<hr class="mt-4" />
+
+<div class="custom-control custom-checkbox">
+ 	{{ Form::checkbox('desactivado', 'null', false, ['class' => 'custom-control-input', 'id' => 'desactivado']) }}
+ 	{{ Form::label('desactivado', 'Desactivar la pregunta, los usuarios ya no podran ver esta pregunta.', ['class' => 'custom-control-label']) }}
+</div>
