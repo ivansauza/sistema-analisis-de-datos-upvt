245237a926c1bc92a9863b70383bc4b58cf21ce6
Ivan Sauza <sauuza@gmail.com>
2018-08-30 01:16:22 -0500

    Ahora los botones de opciones son de tamaño xs

--- resources/views/pregunta/index.blade.php
+++ resources/views/pregunta/index.blade.php
@@ -14,28 +14,37 @@
 		<table class="table table-striped table-hover">
 			<thead>
 				<tr>
+					<th scope="col"></th>
 					<th scope="col">#</th>
 					<th scope="col">Nombre</th>
+					<th scope="col">Nota</th>
+					<th scope="col">Responsable</th>
 					<th scope="col" class="text-right">Opciones</th>
 				</tr>
 			</thead>
 			<tbody>
 				<tr>
+					<td style="width: 15px !important;">
+						<a class="btn btn-sm">
+							<i class="fas fa-expand-arrows-alt"></i>
+						</a>
+					</td>
 					<th scope="row">1</th>
-					<td>Valor de la pregunta</td>
-
+					<td>Número de alumnos que fueron evaluados de Estancias y Estadía</td>
+					<td><small>Periodo establecido. (40 días como máximo)</small></td>
+					<td>PTC</td>
 					<td>
 						<div class="float-right">
-							<a href="{{ route('preguntas.show', 0) }}" class="btn btn-sm btn-outline-primary mt-1">
+							<a href="{{ route('preguntas.show', 0) }}" class="btn btn-xs btn-outline-primary mt-1">
 								<i class="fas fa-eye"></i>
 							</a>
 
-							<a href="{{ route('preguntas.edit', 0) }}" class="btn btn-sm btn-outline-warning mt-1">
+							<a href="{{ route('preguntas.edit', 0) }}" class="btn btn-xs btn-outline-warning mt-1">
 								<i class="fas fa-pencil-alt"></i>
 							</a>
 
 							{{ Form::open(['route' => ['preguntas.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
-								<button type="submit" class="btn btn-outline-danger btn-sm mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''"><i class="fas fa-trash-alt"></i></button>
+								<button type="submit" class="btn btn-outline-danger btn-xs mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''"><i class="fas fa-trash-alt"></i></button>
 							{{ Form::close() }}
 						</div>
 					</td>
