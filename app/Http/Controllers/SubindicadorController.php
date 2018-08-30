908f1f4c6b878983b04d8855ceed22ebab5270ba
Ivan Sauza <sauuza@gmail.com>
2018-08-30 01:19:56 -0500

    Se añadio el modulo de Subindicador

---
+++ app/Http/Controllers/SubindicadorController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Subindicador;
+use Illuminate\Http\Request;
+
+class SubindicadorController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Subindicador  $subindicador
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Subindicador $subindicador)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Subindicador  $subindicador
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Subindicador $subindicador)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Subindicador  $subindicador
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Subindicador $subindicador)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Subindicador  $subindicador
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Subindicador $subindicador)
+    {
+        //
+    }
+}
---
+++ database/factories/SubindicadorFactory.php
@@ -0,0 +1,9 @@
+<?php
+
+use Faker\Generator as Faker;
+
+$factory->define(App\Subindicador::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
