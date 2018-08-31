ce81b2def479257400432c36c73a85819c44c9a5
Ivan Sauza <sauuza@gmail.com>
2018-08-31 12:14:42 -0500

    Mejoras a la vista

--- resources/views/forms/pregunta.blade.php
+++ resources/views/forms/pregunta.blade.php
@@ -1,23 +1,28 @@
-<div class="form-group">
+<div class="form-group mb-4">
 	{{ Form::label('nombre') }}
 	{{ Form::text('nombre', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa la pregunta']) }}
 </div>
 
-<div class="form-group">
-	{{ Form::label('nota') }}
-	{{ Form::textarea('nota', null, ['class' => 'form-control']) }}
-</div>
+<div class="row">
+	<div class="col-7">
+		<div class="form-group">
+			{{ Form::label('rol_id', 'Rol') }}
+			{{ Form::select('rol_id', ['a' => 'Maestro', 'b' => 'Secretaria', 'c' => 'Director'], null, ['class' => 'form-control']) }}
+			<small class="form-text text-muted">Seleccione el Rol de Usuario que podra llenar la información a esta pregunta.</small>
+		</div>
 
-<div class="form-group">
-	{{ Form::label('rol_id', 'Rol') }}
-	{{ Form::select('rol_id', ['a' => 'Maestro', 'b' => 'Secretaria', 'c' => 'Director'], null, ['class' => 'form-control']) }}
-	<small class="form-text text-muted">Seleccione el Rol de Usuario que podra llenar la información a esta pregunta.</small>
-</div>
-
-<div class="form-group">
-	{{ Form::label('programa_id', 'Programa Educativo') }}
-	{{ Form::select('programa_id', ['a' => 'Ingenieria en Informatica', 'b' => 'Maestria en Administracion', 'c' => 'Ingenieria Inddustrial'], null, ['class' => 'form-control', 'multiple' => true]) }}
-	<small class="form-text text-muted">Puede seleccionar varios programas presionando la tecla "control" + "click".</small>
+		<div class="form-group">
+			{{ Form::label('programa_id', 'Programa Educativo') }}
+			{{ Form::select('programa_id', ['a' => 'Ingenieria en Informatica', 'b' => 'Maestria en Administracion', 'c' => 'Ingenieria Inddustrial'], null, ['class' => 'form-control', 'multiple' => true]) }}
+			<small class="form-text text-muted">Puede seleccionar varios programas presionando la tecla "control" + "click".</small>
+		</div>
+	</div>
+	<div class="col">
+		<div class="form-group">
+			{{ Form::label('nota') }}
+			{{ Form::textarea('nota', null, ['class' => 'form-control']) }}
+		</div>
+	</div>
 </div>
 
 <hr class="mt-4" />
