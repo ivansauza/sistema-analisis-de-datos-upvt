e2d164c658dc3479491a6f577ac93e9e3998b2b5
Ivan Sauza <sauuza@gmail.com>
2018-08-31 12:02:53 -0500

    El campo apellidos ahora puede ser nulo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -16,7 +16,7 @@ class CreateUsersTable extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->increments('id');
             $table->string('nombre');
-            $table->string('apellidos');
+            $table->string('apellidos')->nullable();
 
             $table->string('email')->unique();
             $table->string('password');
