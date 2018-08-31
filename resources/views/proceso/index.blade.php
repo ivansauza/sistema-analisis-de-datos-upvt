72eb0fa683c79bb8d94443e55e0c183ecec196a9
Ivan Sauza <sauuza@gmail.com>
2018-08-30 20:08:42 -0500

    Mejoras a la vista

--- resources/views/proceso/index.blade.php
+++ resources/views/proceso/index.blade.php
@@ -10,6 +10,8 @@
 @endsection
 
 @section('content')
+
+{{-- Proceso --}}
 <div class="card card-default">
 	<div class="card-header">
 		<strong>
@@ -21,7 +23,7 @@
 				<i class="fas fa-expand-arrows-alt"></i>
 			</a>
 
-			<a class="btn btn-sm btn-success mt-1" data-toggle="tooltip" data-placement="top" title="Agregar Indicador">
+			<a href="{{ route('indicadores.create', ['proceso_id' => 3]) }}" class="btn btn-sm btn-success mt-1" data-toggle="tooltip" data-placement="top" title="Agregar Indicador">
 				<i class="fas fa-plus"></i>
 			</a>
 
@@ -41,6 +43,7 @@
 
 	<div class="card-body px-0 py-3">
 
+		{{-- Indicador --}}
 		<div class="card card-default border-left-0 border-right-0 rounded-0 mb-3">
 			<div class="card-header">
 				Realización y Evaluación de Estancia y/o Estadía
@@ -50,7 +53,7 @@
 						<i class="fas fa-expand-arrows-alt"></i>
 					</a>
 
-					<a class="btn btn-sm btn-outline-success mt-1" data-toggle="tooltip" data-placement="top" title="Agregar Subindicador">
+					<a href="{{ route('subindicadores.create', ['indicador_id' => 3]) }}" class="btn btn-sm btn-outline-success mt-1" data-toggle="tooltip" data-placement="top" title="Agregar Subindicador">
 						<i class="fas fa-plus"></i>
 					</a>
 
@@ -69,6 +72,8 @@
 			</div>
 
 			<div class="card-body px-0 py-3">
+
+				{{-- Subindicadores --}}
 				<ul class="list-group list-group-flush">
 					<li class="list-group-item">
 						Eficiencia de la validación de Estancias
@@ -91,31 +96,12 @@
 							{{ Form::close() }}
 						</div>
 					</li>
-					<li class="list-group-item">
-						Eficiencia del seguimiento
-
-						<div class="float-right p-0 m-0">
-							<a class="btn btn-xs">
-								<i class="fas fa-expand-arrows-alt"></i>
-							</a>
-
-							<a href="{{ route('procesos.show', 0) }}" class="btn btn-xs btn-outline-primary mt-1">
-								<i class="fas fa-eye"></i>
-							</a>
-
-							<a href="{{ route('procesos.edit', 0) }}" class="btn btn-xs btn-outline-warning mt-1">
-								<i class="fas fa-pencil-alt"></i>
-							</a>
-
-							{{ Form::open(['route' => ['procesos.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
-								<button type="submit" class="btn btn-outline-danger btn-xs mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''"><i class="fas fa-trash-alt"></i></button>
-							{{ Form::close() }}
-						</div>
-					</li>
 				</ul>
+
 			</div>
 		</div>
 
 	</div>
 </div>
+
 @endsection
