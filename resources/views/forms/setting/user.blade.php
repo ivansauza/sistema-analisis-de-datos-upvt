470b898a87890c926bcdb61df49240de2dee7134
Ivan Sauza <sauuza@gmail.com>
2018-08-31 23:18:04 -0500

    Se mejoro la vista y se corrigiron textos

--- resources/views/forms/setting/user.blade.php
+++ resources/views/forms/setting/user.blade.php
@@ -1,5 +1,18 @@
 <div class="row">
 	<div class="col">
+		<div class="card align-self-middle d-flex w-100 mb-4">
+			<div class="card-body">
+				<div class="row">
+					<div class="col-md-3 text-center text-lg-left">
+						<img class="rounded-circle img-fluid" src="https://www.gravatar.com/avatar/de85d9bc74935247ebceed66c9620050?d=mm&amp;s=128" alt="User profile picture">
+					</div>
+					<div class="col-md-9 mt-3 mt-lg-0 text-center text-lg-left">
+						<h1 class="font-weight-light">Ivan Sauza</h1>
+						<p class="text-muted">sauuza@gmail.com</p>
+					</div>
+				</div>
+			</div>
+		</div>
 		<div class="form-group">
 			{{ Form::label('nombre') }}
 			{{ Form::text('nombre', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa el nombre']) }}
@@ -18,7 +31,7 @@
 
 		<div class="form-group">
 			{{ Form::label('password', 'Contraseña nueva') }}
-			{{ Form::text('password', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa la nueva contaseña']) }}
+			{{ Form::text('password', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa la contaseña nueva']) }}
 		</div>
 
 		<div class="form-group">
