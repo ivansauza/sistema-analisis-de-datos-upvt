77fef510522217a3b87d30e224e4f23b9e542a65
Ivan Sauza <sauuza@gmail.com>
2018-09-01 19:38:34 -0500

    Se agrego el input para elejir el periodo del análisis

--- resources/views/forms/analisis.blade.php
+++ resources/views/forms/analisis.blade.php
@@ -1,4 +1,10 @@
 <div class="form-group">
+	{{ Form::label('periodo_id', 'Periodo') }}
+	{{ Form::select('periodo_id', ['a' => 'Enero-Abril 2018', 'b' => 'Mayo-Agosto 2018'], null, ['class' => 'form-control', 'multiple' => false]) }}
+	<small class="form-text text-muted">Puede seleccionar varios items presionando la tecla "control" + "click".</small>
+</div>
+
+<div class="form-group">
 	{{ Form::label('pregunta[]', 'Número de alumnos que fueron evaluados en el de Estancias y Estadía') }}
 	{{ Form::text('pregunta[]', null, ['class' => 'form-control', 'required']) }}
 	<small class="form-text text-muted">Periodo establecido. (40 días como máximo)</small>
