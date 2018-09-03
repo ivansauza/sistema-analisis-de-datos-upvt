b7a15ef78524a14717fd44931f0902d94168fa39
Ivan Sauza <sauuza@gmail.com>
2018-09-03 00:24:21 -0500

    Se agregarón mejoras a la vista

--- resources/views/encuesta/index.blade.php
+++ resources/views/encuesta/index.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.admin')
 
 @section('header')
-	<h1 class="h3">Encuestas <small>Información requerida para el analisis de datos.</small></h1>
+	<h1 class="h3">Encuestas</h1>
 	<div class="btn-toolbar mb-2 mb-md-0">
 		<div class="btn-group mr-2">
 			<a href="{{ route('encuestas.create') }}" class="btn btn-outline-primary btn-sm">Nuevo</a>
@@ -25,7 +25,31 @@
 		<tbody>
 			<tr>
 				<th scope="row">1</th>
-				<td>Adrian Gonzales</td>
+				<td>Adrían Gonzáles</td>
+				<td>Mayo-Agosto 2018</td>
+				<td>Ingeniería en Informática</td>
+				<td class="text-center">
+					<span class="badge badge-pill badge-primary">SI</span>
+				</td>
+				<td>
+					<div class="float-right">
+						<a href="{{ route('encuestas.show', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
+							<i class="fas fa-eye"></i>
+						</a>
+
+						<a href="{{ route('encuestas.edit', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
+							<i class="fas fa-pencil-alt"></i>
+						</a>
+
+						{{ Form::open(['route' => ['encuestas.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
+							<button type="submit" class="btn btn-sm text-secondary btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+						{{ Form::close() }}
+					</div>
+				</td>
+			</tr>
+			<tr>
+				<th scope="row">2</th>
+				<td>Adrían Gonzáles</td>
 				<td>Enero-Abril 2018</td>
 				<td>Ingeniería en Informática</td>
 				<td class="text-center">
