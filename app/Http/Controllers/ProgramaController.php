0f9edc9835042d26d7534712f9bee6d3290eace9
Ivan Sauza <sauuza@gmail.com>
2018-09-03 21:24:33 -0500

    Se agregarón todas las funciones del controlador

--- app/Http/Controllers/ProgramaController.php
+++ app/Http/Controllers/ProgramaController.php
@@ -4,87 +4,54 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 
+use App\Programa;
+use App\Http\Requests\ProgramaRequest;
+
 class ProgramaController extends Controller
 {
-    /**
-     * Display a listing of the resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-
-    /*public function __construct()
-    {
-        $this->middleware('auth');
-    }*/
-
     public function index()
     {
-        return view('programa/index');
+        $programas = Programa::get();
+
+        return view('programa/index', compact('programas'));
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
     public function create()
     {
         return view('programa/create');
     }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
+    public function store(ProgramaRequest $request)
     {
-        //
+        $programa = Programa::create($request->all());
+
+        return redirect()->route('programas.edit', $programa->id)
+            ->with('info', 'Programa guardado con éxito');
     }
 
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
+    public function show(Programa $programa)
     {
-        return view('programa/show');
+        return view('programa/show', compact('programa'));
     }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
+    public function edit(Programa $programa)
     {
-        return view('programa/edit');
+        return view('programa/edit', compact('programa'));
     }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, $id)
+    public function update(ProgramaRequest $request, Programa $programa)
     {
-        //
+        $programa->update($request->all());
+
+        return redirect()->back()
+            ->with('info', 'Programa actualizado con éxito');
     }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
+    public function destroy(Programa $programa)
     {
-        //
+        $programa->delete();
+
+        return redirect()->route('programas.index')
+            ->with('info', 'Programa eliminado con éxito');
     }
 }
