29661627e2c5e5f02ba84b32d3f546eaf885d606
Ivan Sauza <sauuza@gmail.com>
2018-09-05 16:15:05 -0500

    Documentando código

--- app/Http/Controllers/ProgramaController.php
+++ app/Http/Controllers/ProgramaController.php
@@ -14,6 +14,11 @@ class ProgramaController extends Controller
         $this->middleware('auth');
     }
     
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function index()
     {
         $programas = Programa::get();
@@ -21,44 +26,79 @@ class ProgramaController extends Controller
         return view('programa/index', compact('programas'));
     }
 
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
     public function create()
     {
         return view('programa/create');
     }
 
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
     public function store(ProgramaRequest $request)
     {
         $programa = Programa::create($request->all());
 
         return redirect()->route('programas.edit', $programa->id)
-            ->with('info', 'Programa guardado con éxito');
+            ->with('info', ['type' => 'success', 'message' => 'Programa guardado con éxito']);
     }
 
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Periodo  $periodo
+     * @return \Illuminate\Http\Response
+     */
     public function show(Programa $programa)
     {
         return view('programa/show', compact('programa'));
     }
 
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Periodo  $periodo
+     * @return \Illuminate\Http\Response
+     */
     public function edit(Programa $programa)
     {
         return view('programa/edit', compact('programa'));
     }
 
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Periodo  $periodo
+     * @return \Illuminate\Http\Response
+     */
     public function update(ProgramaRequest $request, $id)
     {
         $programa = Programa::findOrFail($id);
         $programa->update($request->all());
 
         return redirect()->back()
-            //->with('info', 'Programa actualizado con éxito');
             ->with('info', ['type' => 'success', 'message' => 'Programa actualizado con éxito']);
     }
 
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Periodo  $periodo
+     * @return \Illuminate\Http\Response
+     */
     public function destroy(Programa $programa)
     {
         $programa->delete();
 
         return redirect()->route('programas.index')
-            ->with('info', 'Programa eliminado con éxito');
+            ->with('info', ['type' => 'success', 'message' => 'Programa eliminado con éxito']);
     }
 }
