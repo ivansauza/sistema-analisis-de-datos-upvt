f7789e1601c73ec7e50afdcec9c24c8fdb68332b
Ivan Sauza <sauuza@gmail.com>
2018-09-06 00:21:38 -0500

    Se agregarón mejoras a la vista

--- resources/views/periodo/index.blade.php
+++ resources/views/periodo/index.blade.php
@@ -18,7 +18,11 @@
 	<h1 class="h3">Periodos</h1>
 	<div class="btn-toolbar mb-2 mb-md-0">
 		<div class="btn-group mr-2">
-			<a href="{{ route('periodos.create') }}" class="btn btn-outline-primary btn-sm">Crear Periodo</a>
+			@if(auth()->user()->programas()->where('predeterminado', '=', 1)->first())
+				<a href="{{ route('periodos.create', ['programa_id' => auth()->user()->programas()->where('predeterminado', '=', 1)->first()->id]) }}" class="btn btn-outline-primary btn-sm">Crear Periodo</a>
+			@endif
+			
+			@include('periodo.fragments.view_menu')
 		</div>
 	</div>
 @endsection
@@ -27,59 +31,72 @@
 	
 	@include('fragments.alert')
 
-	<div class="card card-default">
-		<table class="table table-hover">
-			<thead class="thead-light">
-				<tr>
-					<th scope="col"></th>
-					<th scope="col" class="text-center">ID</th>
-					<th scope="col">Clave</th>
-					<th scope="col" class="text-center">Estado</th>
-					<th scope="col" class="text-center">Actual</th>
-					<th scope="col" class="text-right">Opciones</th>
-				</tr>
-			</thead>
-			<tbody>
-				@foreach($periodos as $periodo)
-					<tr>
-						<td style="width: 15px !important;">
-							<a class="btn btn-sm">
-								<i class="fas fa-expand-arrows-alt"></i>
-							</a>
-						</td>
-						<td class="text-center">{{ $periodo->id }}</td>
-						<td>
-							{{ $periodo->clave }} {{ $periodo->anio }}
-						</td>
-						<td class="text-center">
-							<span class="badge badge-pill badge-{{ $periodo->estado ? 'success' : 'danger' }}">
-								{{ $periodo->estado ? 'Abierto' : 'Cerrado' }}
-							</span>
-						</td>
-						<td class="text-center">
-							<span class="badge badge-pill badge-{{ $periodo->actual ? 'primary' : 'secondary' }}">
-								{{ $periodo->actual ? 'SI' : 'NO' }}
-							</span>
-						</td>
-						<td>
-							<div class="float-right">
-								<a href="{{ route('periodos.show', $periodo->id) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
-									<i class="fas fa-eye"></i>
-								</a>
+	@if($programas->get()->isEmpty())
+		<h6 class="text-center text-muted">
+			No tiene Programas Educativos asignados.
+		</h6>
+	@else
+		@if(!$programas->where('predeterminado', '=', 1)->first())
+			<h6 class="text-center text-muted">
+				Selecciona un programa educativo como predeterminado.
+			</h6>
+		@else
+			<div class="card card-default">
+				<table class="table table-hover">
+					<thead class="thead-light">
+						<tr>
+							<th scope="col"></th>
+							<th scope="col" class="text-center">ID</th>
+							<th scope="col">Clave</th>
+							<th scope="col" class="text-center">Estado</th>
+							<th scope="col" class="text-center">Actual</th>
+							<th scope="col" class="text-right">Opciones</th>
+						</tr>
+					</thead>
+					<tbody>
+						@foreach($programas->where('predeterminado', '=', 1)->first()->periodos as $periodo)
+							<tr>
+								<td style="width: 15px !important;">
+									<a class="btn btn-sm">
+										<i class="fas fa-expand-arrows-alt"></i>
+									</a>
+								</td>
+								<td class="text-center">{{ $periodo->id }}</td>
+								<td>
+									{{ $periodo->clave }} {{ $periodo->anio }}
+								</td>
+								<td class="text-center">
+									<span class="badge badge-pill badge-{{ $periodo->estado ? 'success' : 'danger' }}">
+										{{ $periodo->estado ? 'Abierto' : 'Cerrado' }}
+									</span>
+								</td>
+								<td class="text-center">
+									<span class="badge badge-pill badge-{{ $periodo->actual ? 'primary' : 'secondary' }}">
+										{{ $periodo->actual ? 'SI' : 'NO' }}
+									</span>
+								</td>
+								<td>
+									<div class="float-right">
+										<a href="{{ route('periodos.show', $periodo->id) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
+											<i class="fas fa-eye"></i>
+										</a>
 
-								<a href="{{ route('periodos.edit', $periodo->id) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
-									<i class="fas fa-pencil-alt"></i>
-								</a>
+										<a href="{{ route('periodos.edit', $periodo->id) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
+											<i class="fas fa-pencil-alt"></i>
+										</a>
 
-								{{ Form::open(['route' => ['periodos.destroy', $periodo->id], 'method' => 'DELETE', 'class' => 'd-inline']) }}
-									<button type="submit" class="btn btn-sm text-danger btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
-								{{ Form::close() }}
-							</div>
-						</td>
-					</tr>
-				@endforeach
-			</tbody>
-		</table>
-	</div>
+										{{ Form::open(['route' => ['periodos.destroy', $periodo->id], 'method' => 'DELETE', 'class' => 'd-inline']) }}
+											<button type="submit" class="btn btn-sm text-danger btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+										{{ Form::close() }}
+									</div>
+								</td>
+							</tr>
+						@endforeach
+					</tbody>
+				</table>
+			</div>
+		@endif
+
+	@endif
 	
 @endsection
