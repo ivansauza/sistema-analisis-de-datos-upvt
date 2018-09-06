737427ecfedf2ec348afc46d2e2926ee8e7dc835
Ivan Sauza <sauuza@gmail.com>
2018-09-06 00:14:05 -0500

    Factories agregadas

--- database/factories/actividadFactory.php
+++ database/factories/actividadFactory.php
@@ -2,7 +2,7 @@
 
 use Faker\Generator as Faker;
 
-$factory->define(App\actividad::class, function (Faker $faker) {
+$factory->define(App\Actividad::class, function (Faker $faker) {
     return [
         //
     ];
