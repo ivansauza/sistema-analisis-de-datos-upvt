dc3fa6c7ab197312ddee6642ee67e065b51b975d
Ivan Sauza <sauuza@gmail.com>
2018-09-07 21:09:18 -0500

    Se agregrón mejoras a la vista

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -18,48 +18,58 @@
 	<h1 class="h3">Usuarios</h1>
 	<div class="btn-toolbar mb-2 mb-md-0">
 		<div class="btn-group mr-2">
-			<a href="{{ route('users.create') }}" class="btn btn-outline-primary btn-sm">Nuevo</a>
+				<a href="{{ route('users.create') }}" class="btn btn-outline-primary btn-sm">
+					Crear Usuario
+				</a>
+				@include('user.fragments.view_menu')
 		</div>
 	</div>
 @endsection
 
 @section('content')
-<div class="card card-default">
-	<table class="table table-striped table-hover">
-		<thead>
-			<tr>
-				<th scope="col">#</th>
-				<th scope="col">Nombre</th>
-				<th scope="col">Correo elestrónico</th>
-				<th scope="col">Actividades</th>
-				<th scope="col" class="text-right">Opciones</th>
-			</tr>
-		</thead>
-		<tbody>
-			<tr>
-				<th scope="row">1</th>
-				<td>Adrian Gonzales</td>
-				<td>adrian@upvt.com</td>
-				<td>
-					<a href="{{ route('users.actividades', 0) }}" class="text-secondary">Ver historial</a>
-				</td>
-				<td>
-					<div class="float-right">
-						<a href="{{ route('users.show', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
-							<i class="fas fa-eye"></i>
-						</a>
 
-						<a href="{{ route('users.edit', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
-							<i class="fas fa-pencil-alt"></i>
-						</a>
+	@include('fragments.alert')
 
-						{{ Form::open(['route' => ['users.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
-							<button type="submit" class="btn btn-sm text-secondary btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
-						{{ Form::close() }}
-					</div>
-				</td>
-			</tr>
-		</tbody>
-	</table>
-</div>
+	<div class="card card-default">
+		<table class="table table-striped table-hover">
+			<thead class="thead-light">
+				<tr>
+					<th scope="col">#</th>
+					<th scope="col">Nombre</th>
+					<th scope="col">Correo elestrónico</th>
+					<th scope="col">Actividades</th>
+					<th scope="col" class="text-right">Opciones</th>
+				</tr>
+			</thead>
+			<tbody>
+				@foreach($users as $user)
+					<tr>
+						<th scope="row">1</th>
+						<td>Adrian Gonzales</td>
+						<td>adrian@upvt.com</td>
+						<td>
+							<a href="{{ route('users.actividades', 0) }}" class="text-secondary">Ver historial</a>
+						</td>
+						<td>
+							<div class="float-right">
+								<a href="{{ route('users.show', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
+									<i class="fas fa-eye"></i>
+								</a>
+
+								<a href="{{ route('users.edit', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
+									<i class="fas fa-pencil-alt"></i>
+								</a>
+
+								{{ Form::open(['route' => ['users.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
+									<button type="submit" class="btn btn-sm text-secondary btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+								{{ Form::close() }}
+							</div>
+						</td>
+					</tr>
+				@endforeach
+			</tbody>
+		</table>
+	</div>
+
+
 @endsection
