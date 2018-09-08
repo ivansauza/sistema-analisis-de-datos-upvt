4b19c7b41d2513ecc22fc0c7f9b50d3d666448c1
Ivan Sauza <sauuza@gmail.com>
2018-09-08 13:08:34 -0500

    Se agregarón mejoras al código

--- resources/views/forms/setting/user.blade.php
+++ resources/views/forms/setting/user.blade.php
@@ -3,8 +3,15 @@
 		<img class="rounded-circle img-fluid" src="https://www.gravatar.com/avatar/de85d9bc74935247ebceed66c9620050?d=mm&amp;s=128" alt="User profile picture">
 	</div>
 	<div class="col-md-9 mt-3 mt-lg-0 text-center text-lg-left">
-		<h2 class="font-weight-light">Ivan Sauza <small>Director</small></h2>
-		<p class="text-muted">sauuza@gmail.com</p>
+		<h2 class="font-weight-light">
+			{{ $user->name }} {{ $user->apellidos }} 
+			<small>
+				{{ $user->roles()->first()->name }}
+			</small>
+		</h2>
+		<p class="text-muted">
+			{{ $user->email }}
+		</p>
 	</div>
 </div>
 
@@ -12,11 +19,11 @@
 	<tbody>
 		<tr>
 			<td class="text-right">Fecha de registro: </td>
-			<td>Agosto 31, 2018 00:16</td>
+			<td>{{ $user->created_at }}</td>
 		</tr>
 		<tr>
 			<td class="text-right">Fecha de edición: </td>
-			<td>Agosto 31, 2018 00:16</td>
+			<td>{{ $user->updated_at }}</td>
 		</tr>
 	</tbody>
 </table>
@@ -25,33 +32,48 @@
 <div class="row">
 	<div class="col-xs-12 col-md">
 		<div class="form-group">
-			{{ Form::label('nombre') }}
-			{{ Form::text('nombre', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa el nombre']) }}
+			{{ Form::label('name') }}
+			{{ Form::text('name', null, ['class' => 'form-control', 'placeholder' => 'Ingresa el nombre']) }}
+			@if ($errors->has('name'))
+				<span class="badge badge-danger">{{ $errors->first('name') }}</span>
+			@endif
 		</div>
 		<div class="form-group">
 			{{ Form::label('apellidos') }}
-			{{ Form::text('apellidos', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa los apellidos']) }}
+			{{ Form::text('apellidos', null, ['class' => 'form-control', 'placeholder' => 'Ingresa los apellidos']) }}
+			@if ($errors->has('apellidos'))
+				<span class="badge badge-danger">{{ $errors->first('apellidos') }}</span>
+			@endif
 		</div>
 	</div>
 	<div class="col-xs-12 col-md">
 		<div class="form-group">
 			{{ Form::label('email', 'Correo Electrónico') }}
-			{{ Form::text('email', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa el correo electrónico']) }}
+			{{ Form::email('email', null, ['class' => 'form-control', 'placeholder' => 'Ingresa el correo electrónico']) }}
+			@if ($errors->has('email'))
+				<span class="badge badge-danger">{{ $errors->first('email') }}</span>
+			@endif
 		</div>
 		<div class="form-group">
 			{{ Form::label('password', 'Contraseña nueva') }}
-			{{ Form::text('password', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa la contaseña nueva']) }}
+			{{ Form::password('password', ['class' => 'form-control', 'placeholder' => 'Ingresa la contaseña nueva']) }}
+			@if ($errors->has('password'))
+				<span class="badge badge-danger">{{ $errors->first('password') }}</span>
+			@endif
 		</div>
 		<div class="form-group">
-			{{ Form::label('password', 'Confirmar contraseña nueva') }}
-			{{ Form::text('password', null, ['class' => 'form-control', 'required', 'placeholder' => 'Confirma la contraseña nueva']) }}
+			{{ Form::label('password_confirmation', 'Confirmar contraseña nueva') }}
+			{{ Form::password('password_confirmation', ['class' => 'form-control', 'placeholder' => 'Confirma la contraseña nueva']) }}
 		</div>
 
 		<hr />
 
 		<div class="form-group">
 			{{ Form::label('current_password', 'Contraseña actual') }}
-			{{ Form::text('current_password', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa la contraseña actual']) }}
+			{{ Form::password('current_password', ['class' => 'form-control', 'placeholder' => 'Ingresa la contraseña actual']) }}
+			@if ($errors->has('current_password'))
+				<span class="badge badge-danger">{{ $errors->first('current_password') }}</span>
+			@endif
 		</div>
 	</div>
 </div>
