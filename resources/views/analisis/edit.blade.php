2cdc057b448e2eadd36b5417dabad510f3f31fbb
Ivan Sauza <sauuza@gmail.com>
2018-09-10 12:54:27 -0500

    Vista agregada

---
+++ resources/views/analisis/edit.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.admin')
+
+@section('header')
+	<h1 class="h3">Análsis <small>Editar</small></h1>
+	<div class="btn-toolbar mb-2 mb-md-0">
+		<div class="btn-group mr-2">
+			<a href="{{ route('analisis.index') }}" class="btn btn-outline-secondary btn-sm">Regresar</a>
+		</div>
+	</div>
+@endsection
+
+@section('content')
+	
+	@include('fragments.alert')
+
+	<div class="card">
+		{{ Form::model($analisis, ['route' => ['periodos.update', $analisis->id], 'method' => 'PUT']) }}
+
+			@include('forms.analisis.edit')
+
+			<div class="card-footer">
+				{{ Form::submit('Actualizar', ['class' => 'btn btn-block btn-warning']) }}
+			</div>
+
+		{{ Form::close() }}
+	</div>
+	
+@endsection
