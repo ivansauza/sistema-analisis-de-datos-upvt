6046e53d2738bb9e36bb68af327a84facbe99168
Ivan Sauza <sauuza@gmail.com>
2018-09-11 13:11:06 -0500

    Request agregado para verificar si el periodo enviado es correcto

---
+++ app/Http/Requests/AnalisisCreateRequest.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+use App\Periodo;
+use App\Programa;
+
+class AnalisisCreateRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        /**
+         * Verificar si el periodo enviado tiene estado 1(Abierto), si
+         * pertenece al programa predeterminado con respecto al usuario 
+         * actualmente logeado, si ya tiene una encuesta creada.
+         */
+        Periodo::where('estado', '=', 1)
+            ->where('programa_id', '=', Programa::getPredeterminado()->id)
+            ->whereNotIn('id', auth()->user()->getEncuestasExistsPeriodos())
+            ->findOrFail(request()->input('periodo_id'));
+
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            //
+        ];
+    }
+}
