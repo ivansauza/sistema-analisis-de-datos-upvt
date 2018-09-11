3fa59135b8fd009fb142ed3d365d9c5f787c41a6
Ivan Sauza <sauuza@gmail.com>
2018-09-10 22:14:13 -0500

    Mensajes de error agregados

--- resources/views/forms/indicador.blade.php
+++ resources/views/forms/indicador.blade.php
@@ -3,18 +3,27 @@
 		<div class="form-group">
 			{{ Form::label('nombre') }}
 			{{ Form::text('nombre', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa el nombre']) }}
+			@if ($errors->has('nombre'))
+				<span class="badge badge-danger">{{ $errors->first('nombre') }}</span>
+			@endif
 		</div>
 
 		<div class="form-group">
 			{{ Form::label('proceso_id', 'Proceso') }}
 			{{ Form::select('proceso_id', $procesos, request()->input('proceso_id'), ['class' => 'form-control', 'placeholder' => 'Seleccionar ...']) }}
 			<small class="form-text text-muted">Puede seleccionar varios items presionando la tecla "control" + "click".</small>
+			@if ($errors->has('proceso_id'))
+				<span class="badge badge-danger">{{ $errors->first('proceso_id') }}</span>
+			@endif
 		</div>
 	</div>
 	<div class="col">
 		<div class="form-group">
 			{{ Form::label('nota') }}
 			{{ Form::textarea('nota', null, ['class' => 'form-control']) }}
+			@if ($errors->has('nota'))
+				<span class="badge badge-danger">{{ $errors->first('nota') }}</span>
+			@endif
 		</div>
 	</div>
 </div>
