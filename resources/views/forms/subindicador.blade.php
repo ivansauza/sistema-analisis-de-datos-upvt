cc9a08d5c20476eb45e42236a954832f6e68c0b4
Ivan Sauza <sauuza@gmail.com>
2018-09-10 22:14:34 -0500

    Mensajes de error agregados

--- resources/views/forms/subindicador.blade.php
+++ resources/views/forms/subindicador.blade.php
@@ -2,33 +2,40 @@
 	<div class="col">
 		<div class="form-group">
 			{{ Form::label('nombre') }}
-			{{ Form::text('nombre', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa el nombre']) }}
+			{{ Form::text('nombre', null, ['class' => 'form-control', 'placeholder' => 'Ingresa el nombre']) }}
+			@if ($errors->has('nombre'))
+				<span class="badge badge-danger">{{ $errors->first('nombre') }}</span>
+			@endif
 		</div>
 
 		<div class="form-group">
 			{{ Form::label('valor_meta', 'Valor meta') }}
 			<div class="input-group mb-3">
-				{{ Form::number('valor_meta', null, ['class' => 'form-control', 'required', 'placeholder' => 'Ingresa el valor meta', 'min' => '1', 'max' => '100']) }}
+				{{ Form::number('valor_meta', null, ['class' => 'form-control', 'placeholder' => 'Ingresa el valor meta', 'min' => '0', 'max' => '100']) }}
 				<div class="input-group-append">
 					<span class="input-group-text">%</span>
 				</div>
 			</div>
+			@if ($errors->has('valor_meta'))
+				<span class="badge badge-danger">{{ $errors->first('valor_meta') }}</span>
+			@endif
 		</div>
 
 		<div class="form-group">
 			{{ Form::label('indicador_id', 'Indicador') }}
-			{{ Form::select('indicador_id', ['a' => 'Realización y Evaluación de Estancia y/o Estadía'], null, ['class' => 'form-control', 'required']) }}
+			{{ Form::select('indicador_id', $indicadores, request()->input('indicador_id'), ['class' => 'form-control']) }}
+			@if ($errors->has('indicador_id'))
+				<span class="badge badge-danger">{{ $errors->first('indicador_id') }}</span>
+			@endif
 		</div>
-
-		<!--<div class="form-group">
-			{{ Form::label('programa_id', 'Programa Educativo') }}
-			{{ Form::select('programa_id', ['a' => 'Ingeniería Informática', 'b' => 'Maestría en Administración'], null, ['class' => 'form-control', 'multiple' => false, 'required']) }}
-		</div>-->
 	</div>
 	<div class="col">
 		<div class="form-group">
 			{{ Form::label('nota') }}
 			{{ Form::textarea('nota', null, ['class' => 'form-control']) }}
+			@if ($errors->has('nota'))
+				<span class="badge badge-danger">{{ $errors->first('nota') }}</span>
+			@endif
 		</div>
 	</div>
 </div>
@@ -51,22 +58,22 @@
 
 <div class="form-row">
 	<div class="form-group col">
-		{{ Form::select('pregunta_id[]', ['a' => 'Número de alumnos que fueron evaluados de Estancias y Estadía', 'b' => 'Número total de alumnos que cumplieron con el 100% de asesorías'], null, ['class' => 'form-control', 'required']) }}
+		{{ Form::select('pregunta_id[]', ['a' => 'Número de alumnos que fueron evaluados de Estancias y Estadía', 'b' => 'Número total de alumnos que cumplieron con el 100% de asesorías'], null, ['class' => 'form-control']) }}
 	</div>
 
 	<div class="form-group col">
-		{{ Form::select('operacion[]', ['division' => '/', 'multiplicacion' => '*', 'suma' => '+', 'resta' => '-', 'raiz' => '√'], null, ['class' => 'form-control', 'required']) }}
+		{{ Form::select('operacion[]', ['division' => '/', 'multiplicacion' => '*', 'suma' => '+', 'resta' => '-', 'raiz' => '√'], null, ['class' => 'form-control']) }}
 	</div>
 
 	<div class="form-group col">
-		{{ Form::select('pregunta_id[]', ['a' => 'Número de alumnos que fueron evaluados de Estancias y Estadía', 'b' => 'Número total de alumnos que cumplieron con el 100% de asesorías'], null, ['class' => 'form-control', 'required']) }}
+		{{ Form::select('pregunta_id[]', ['a' => 'Número de alumnos que fueron evaluados de Estancias y Estadía', 'b' => 'Número total de alumnos que cumplieron con el 100% de asesorías'], null, ['class' => 'form-control']) }}
 	</div> 
 
 	<div class="form-group col">
-		{{ Form::select('operacion[]', ['division' => '/', 'multiplicacion' => '*', 'suma' => '+', 'resta' => '-', 'raiz' => '√'], null, ['class' => 'form-control', 'required']) }}
+		{{ Form::select('operacion[]', ['division' => '/', 'multiplicacion' => '*', 'suma' => '+', 'resta' => '-', 'raiz' => '√'], null, ['class' => 'form-control']) }}
 	</div>
 
 	<div class="form-group col">
-		{{ Form::text('numero[]', null, ['class' => 'form-control', 'required']) }}
+		{{ Form::text('numero[]', null, ['class' => 'form-control']) }}
 	</div>	
 </div>
