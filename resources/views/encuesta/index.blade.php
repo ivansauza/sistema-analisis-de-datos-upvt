3de38226e9e3fbc482f0781884512c55c008e65c
Ivan Sauza <sauuza@gmail.com>
2018-09-10 22:13:51 -0500

    Ahora se hace el foreach a periodos para despues sacar las ecuestas de el periodo

--- resources/views/encuesta/index.blade.php
+++ resources/views/encuesta/index.blade.php
@@ -36,32 +36,34 @@
 			</tr>
 		</thead>
 		<tbody>
-			@foreach($encuestas as $encuesta)
-				<tr>
-					<th scope="row">{{ $encuesta->id }}</th>
-					<td>{{ $encuesta->user->full_name }}</td>
-					<td>{{ $encuesta->periodo->full_clave }}</td>
-					<td class="text-center">
-						<span class="badge badge-pill badge-{{ $encuesta->finalizado ? 'primary' : 'secondary' }}">
-							{{ $encuesta->finalizado ? 'SI' : 'NO' }}
-						</span>
-					</td>
-					<td>
-						<div class="float-right">
-							<a href="{{ route('encuestas.show', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
-								<i class="fas fa-eye"></i>
-							</a>
+			@foreach($periodos as $periodo)
+				@foreach($periodo->encuestas as $encuesta)
+					<tr>
+						<th scope="row">{{ $encuesta->id }}</th>
+						<td>{{ $encuesta->user->full_name }}</td>
+						<td>{{ $encuesta->periodo->full_clave }}</td>
+						<td class="text-center">
+							<span class="badge badge-pill badge-{{ $encuesta->finalizado ? 'primary' : 'secondary' }}">
+								{{ $encuesta->finalizado ? 'SI' : 'NO' }}
+							</span>
+						</td>
+						<td>
+							<div class="float-right">
+								<a href="{{ route('encuestas.show', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Mostrar">
+									<i class="fas fa-eye"></i>
+								</a>
 
-							<a href="{{ route('encuestas.edit', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
-								<i class="fas fa-pencil-alt"></i>
-							</a>
+								<a href="{{ route('encuestas.edit', 0) }}" class="btn btn-sm text-secondary mt-1" data-toggle="tooltip" data-placement="top" title="Editar">
+									<i class="fas fa-pencil-alt"></i>
+								</a>
 
-							{{ Form::open(['route' => ['encuestas.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
-								<button type="submit" class="btn btn-sm text-secondary btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
-							{{ Form::close() }}
-						</div>
-					</td>
-				</tr>
+								{{ Form::open(['route' => ['encuestas.destroy', 0], 'method' => 'DELETE', 'class' => 'd-inline']) }}
+									<button type="submit" class="btn btn-sm text-secondary btn-transparent mt-1" onclick="! confirm('Confirmar para eliminar el elemento definiticamente.') ? event.preventDefault() : ''" data-toggle="tooltip" data-placement="top" title="Eliminar"><i class="fas fa-trash-alt"></i></button>
+								{{ Form::close() }}
+							</div>
+						</td>
+					</tr>
+				@endforeach
 			@endforeach
 		</tbody>
 	</table>
