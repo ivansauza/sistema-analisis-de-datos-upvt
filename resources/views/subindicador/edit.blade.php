7cab3776aba146ac4c0f10e42f298f471e2f6eb6
Ivan Sauza <sauuza@gmail.com>
2018-09-11 23:40:03 -0500

    Se agregó código para mostrar los items del procedimiento

--- resources/views/subindicador/edit.blade.php
+++ resources/views/subindicador/edit.blade.php
@@ -19,6 +19,63 @@
 
 				@include('forms.subindicador')
 
+				<div class="form-row" id="procedimiento">
+					@foreach($procedimiento as $item)
+						@switch($item['type'])
+							@case('pregunta')
+								<div class="form-group col">
+									{{ Form::hidden("procedimiento_types[]", "pregunta") }}
+									<div class="input-group mb-3">
+										{{ Form::select("procedimiento_values[]", $preguntas, $item['value'], ["class" => "form-control"]) }}
+										<div class="input-group-append">
+											<a href="#" class="btn btn-outline-secondary">
+													<i class="fas fa-arrows-alt-h"></i>
+											</a>
+											<a href="#" class="btn btn-outline-danger" onclick="removeItem(this)">
+												<i class="fas fa-trash"></i>
+											</a>
+										</div>
+									</div>
+								</div>
+								@break
+
+							@case('operacion')
+								<div class="form-group col">
+									{{ Form::hidden("procedimiento_types[]", "operacion") }}
+									<div class="input-group mb-3">
+										{{ Form::select("procedimiento_values[]", ["/" => "División", "*" => "Multiplicación", "+" => "Suma", "-" => "Resta"], $item['value'], ["class" => "form-control"]) }}
+										<div class="input-group-append">
+											<a href="#" class="btn btn-outline-secondary">
+													<i class="fas fa-arrows-alt-h"></i>
+											</a>
+											<a href="#" class="btn btn-outline-danger" onclick="removeItem(this)">
+												<i class="fas fa-trash"></i>
+											</a>
+										</div>
+									</div>
+								</div>
+								@break
+
+							@case('numero')
+								<div class="form-group col">
+									{{ Form::hidden("procedimiento_types[]", "numero") }}
+									<div class="input-group mb-3">
+										{{ Form::text("procedimiento_values[]", $item['value'], ["class" => "form-control"]) }}
+										<div class="input-group-append">
+											<a href="#" class="btn btn-outline-secondary">
+													<i class="fas fa-arrows-alt-h"></i>
+											</a>
+											<a href="#" class="btn btn-outline-danger" onclick="removeItem(this)">
+												<i class="fas fa-trash"></i>
+											</a>
+										</div>
+									</div>
+								</div>
+								@break
+						@endswitch
+					@endforeach
+				</div>
+
 				{{ Form::submit('Actualizar', ['class' => 'btn btn-block btn-warning mt-5']) }}
 
 			{{ Form::close() }}
