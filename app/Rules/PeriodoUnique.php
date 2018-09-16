8bfb224f6f57730f0476c90e9bc578a2f829a573
Ivan Sauza <sauuza@gmail.com>
2018-09-16 16:50:44 -0500

    Se eliminó el atributo programa_id del constructor, código comentado

--- app/Rules/PeriodoUnique.php
+++ app/Rules/PeriodoUnique.php
@@ -4,6 +4,8 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
+use App\Programa;
+
 class PeriodoUnique implements Rule
 {
     protected $id;
@@ -12,14 +14,15 @@ class PeriodoUnique implements Rule
 
     /**
      * Create a new rule instance.
-     *
+     * @param  string  $clave
+     * @param  integer  $id
      * @return void
      */
-    public function __construct($clave, $id, $programa_id)
+    public function __construct($clave, $id)
     {
         $this->clave       = $clave;
         $this->id          = $id;
-        $this->programa_id = $programa_id;
+        $this->programa_id = Programa::getPredeterminado()->id;
     }
 
     /**
@@ -31,6 +34,9 @@ class PeriodoUnique implements Rule
      */
     public function passes($attribute, $value)
     {
+        /**
+         * Determinar si el periodo enviado ya esta en la bd
+         */
         $count = \DB::table('periodos')
             ->where('id', '!=', $this->id)
             ->where('anio', '=', $value)
