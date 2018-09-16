05444c7b3d5eab32ad9a7166a23076d8c5024714
Ivan Sauza <sauuza@gmail.com>
2018-09-16 16:51:01 -0500

    Mejoras simples

--- app/Traits/ProgramasEmptyValidate.php
+++ app/Traits/ProgramasEmptyValidate.php
@@ -7,7 +7,7 @@ trait ProgramasEmptyValidate
 	{
 		$this->middleware(function ($request, $next) 
 		{
-			if( auth()->user()->programas()->where('predeterminado', '=', 1)->get()->isEmpty() ) {
+			if( ! auth()->user()->programas()->where('predeterminado', '=', 1)->first() ) {
 				return redirect()->route('programas.index')
 					->with('info', ['type' => 'danger', 'message' => 'Seleccione un Programa Educativo como predeterminado.']);
 			}
