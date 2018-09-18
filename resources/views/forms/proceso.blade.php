2850524e6d7ff780246ed15937d52da8a4b12c60
Ivan Sauza <sauuza@gmail.com>
2018-09-18 17:57:03 -0500

    Se eliminó el input de programa

--- resources/views/forms/proceso.blade.php
+++ resources/views/forms/proceso.blade.php
@@ -7,14 +7,6 @@
 </div>
 
 <div class="form-group">
-	{{ Form::label('programa_id', 'Programa Educativo') }}
-	{{ Form::select('programa_id', $programas, request()->input('programa_id'), ['class' => 'form-control', 'placeholder' => 'Seleccionar ...']) }}
-	@if ($errors->has('programa_id'))
-		<span class="badge badge-danger">{{ $errors->first('programa_id') }}</span>
-	@endif
-</div>
-
-<div class="form-group">
 	{{ Form::label('nota') }}
 	{{ Form::textarea('nota', null, ['class' => 'form-control']) }}
 	@if ($errors->has('nota'))
