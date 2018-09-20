08b6781b503b4bc664254ebcdf6f96cce8c45f9a
Ivan Sauza <sauuza@gmail.com>
2018-09-20 01:42:33 -0500

    Se agregó un layout para inyectar css a las vistas

--- resources/views/layouts/home.blade.php
+++ resources/views/layouts/home.blade.php
@@ -13,6 +13,7 @@
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="{{ asset('css/bootstrap.min.css') }}">
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    @yield('css')
 </head>
 
 <body>
