1e65db9990cf72f40ad0b8a7242c27841b838098
Ivan Sauza <sauuza@gmail.com>
2018-09-20 01:40:44 -0500

    Todas las rutas fueron escritas de nuevo

--- routes/web.php
+++ routes/web.php
@@ -17,82 +17,78 @@ Route::get('/', function () {
 
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::middleware(['auth'])->group(function () {
 
-Route::get('programas/{programa}/predetermined', 'ProgramaController@predetermined')
-	->name('programas.predetermined');
-
-Route::resource('programas', 'ProgramaController');
-
-Route::post('periodos/posicion/update', 'PeriodoController@posicionUpdate')
-	->name('periodos.posicion.update')
-	->middleware('auth');
-
-Route::resource('periodos', 'PeriodoController')
-	->middleware('auth');
-
-Route::post('preguntas/posicion/update', 'PreguntaController@posicionUpdate')
-	->name('preguntas.posicion.update')
-	->middleware('auth');
-
-Route::resource('preguntas', 'PreguntaController')
-	->middleware('auth');
-
-Route::get('encuestas/select/user', 'EncuestaSelectUser')
-	->name('encuestas.select.user')
-	->middleware('auth');
-
-Route::get('encuestas/select/periodo', 'EncuestaSelectPeriodo')
-	->name('encuestas.select.periodo')
-	->middleware('auth');
-
-Route::resource('encuestas', 'EncuestaController')
-	->middleware('auth');
-
-Route::resource('procesos', 'ProcesoController')
-	->middleware('auth');
-
-Route::resource('indicadores', 'IndicadorController', ['parameters' => [
-	'indicadores' => 'indicador'
-]])
-	->middleware('auth');
-
-Route::resource('subindicadores', 'SubindicadorController', ['parameters' => [
-	'subindicadores' => 'subindicador'
-]])
-	->middleware('auth');
-
-Route::resource('users', 'UserController')
-	->middleware('auth');
-
-Route::get('users/{id}/actividades', 'UserController@actividades')
-	->name('users.actividades');
-
-Route::get('analisis/select', 'AnalisisSelectPeriodo')
-	->name('analisis.select')
-	->middleware('auth');
-
-Route::resource('analisis', 'AnalisisController', ['parameters' => [
-	'analisis' => 'analisis'
-]])
-	->middleware('auth');
-
-Route::get('estadisticas/details', 'EstadisticaController@details')
-	->name('estadisticas.details')
-	->middleware('auth');
-
-Route::resource('estadisticas', 'EstadisticaController')
-	->middleware('auth');
-
-Route::group(['prefix' => 'configuracion'], function(){
-
-	Route::get('user', 'UserSettings')
-		->name('settings.user');
-
-	Route::post('user', 'UserSettingsUpdate')
-		->name('settings.user.update');
+	Route::get('programas/{programa}/predetermined', 'ProgramaController@predetermined')
+		->name('programas.predetermined');
 	
-});
+	Route::resource('programas', 'ProgramaController');
 
-Route::get('actividades', 'ShowActividades')
-	->name('actividades.show');
+	Route::get('/home', 'HomeController@index')
+		->name('home');
+
+	Route::post('periodos/posicion/update', 'PeriodoController@posicionUpdate')
+		->name('periodos.posicion.update');
+	
+	Route::resource('periodos', 'PeriodoController');
+
+	Route::post('preguntas/posicion/update', 'PreguntaController@posicionUpdate')
+		->name('preguntas.posicion.update')
+		->middleware('permission:encuestas.create');
+
+	Route::resource('preguntas', 'PreguntaController');
+
+	Route::get('analisis/select', 'AnalisisSelectPeriodo')
+	->name('analisis.select');
+
+	Route::resource('analisis', 'AnalisisController', ['parameters' => [
+		'analisis' => 'analisis'
+	]]);
+
+	Route::get('encuestas/select/user', 'EncuestaSelectUser')
+		->name('encuestas.select.user')
+		->middleware('permission:encuestas.create');
+
+	Route::get('encuestas/select/periodo', 'EncuestaSelectPeriodo')
+		->name('encuestas.select.periodo');
+
+	Route::resource('encuestas', 'EncuestaController');
+
+	Route::resource('procesos', 'ProcesoController');
+
+	Route::resource('indicadores', 'IndicadorController', ['parameters' => [
+		'indicadores' => 'indicador'
+	]])
+		->middleware('permission:procesos.create');
+
+	Route::resource('subindicadores', 'SubindicadorController', ['parameters' => [
+		'subindicadores' => 'subindicador'
+	]])
+		->middleware('permission:procesos.create');
+
+	Route::get('estadisticas', 'EstadisticaController@index')
+		->name('estadisticas.index')
+		->middleware('permission:estadisticas.index');
+	
+	Route::get('estadisticas/details', 'EstadisticaController@details')
+		->name('estadisticas.details')
+		->middleware('permission:estadisticas.details');
+
+	Route::get('users/{id}/actividades', 'UserController@actividades')
+		->name('users.actividades');
+	
+	Route::resource('users', 'UserController');
+
+	Route::group(['prefix' => 'configuracion'], function () {
+
+		Route::get('user', 'UserSettings')
+			->name('settings.user');
+	
+		Route::post('user', 'UserSettingsUpdate')
+			->name('settings.user.update');
+		
+	});
+	
+	Route::get('actividades', 'ShowActividades')
+		->name('actividades.show');
+});
