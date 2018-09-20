479b849028a2b44810d7976b90e77dc0f4f7eb4b
Ivan Sauza <sauuza@gmail.com>
2018-09-20 01:42:05 -0500

    Mejoras al código

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -18,10 +18,11 @@
 </head>
 
 <body>
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow-sm p-0">
-		<a class="navbar-brand col-sm-3 col-md-3 mr-0 d-xs-none" href="{{ route('home') }}">Análisis de Datos</a>
+	<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top shadow-sm">
+		<a class="navbar-brand d-block d-sm-none" href="{{ route('home') }}">Análisis de Datos</a>
+		<a class="navbar-brand col-sm-3 col-md-3 mr-0 d-none d-sm-block" href="{{ route('home') }}">Análisis de Datos</a>
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
+			<i class="fas fa-bars"></i>
 		</button>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -46,9 +47,12 @@
 						@endforeach
 					</div>
 				</li>
+				<span class="d-block d-sm-none">
+					@include('fragments.menu')
+				</span>
 			</ul>
 
-			<ul class="navbar-nav px-3">
+			<ul class="navbar-nav">
 				<li class="nav-item dropdown">
 					<a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
 						<i class="far fa-user-circle"></i> 
@@ -72,80 +76,7 @@
 			<nav class="col-md-3 d-none d-md-block shadow-sm sidebar" style="background: #fff;">
 				<div class="sidebar-sticky">
 					<ul class="nav flex-column">
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is('home') ?: 'active' }}" href="{{ route('home') }}">
-								<i class="fas fa-home"></i> 
-								Inicio <span class="sr-only">(current)</span>
-							</a>
-						</li>
-
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is('programas.*') ?: 'active' }}" href="{{ route('programas.index') }}">
-								<i class="fas fa-user-graduate"></i> 
-								Programas Educativos
-								<span class="badge badge-primary float-right">{{ auth()->user()->programas()->count() }}</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is('periodos.*') ?: 'active' }}" href="{{ route('periodos.index') }}">
-								<i class="fas fa-calendar-alt"></i> 
-								Periodos
-							</a>
-						</li>
-
-						<div class="dropdown-divider"></div>
-
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is('preguntas.*') ?: 'active' }}" href="{{ route('preguntas.index') }}">
-								<i class="far fa-question-circle"></i> 
-								Preguntas
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is('encuestas.*') ?: 'active' }}" href="{{ route('encuestas.index') }}">
-								<i class="fas fa-align-left"></i> 
-								Encuestas
-							</a>
-						</li>
-						
-						<li class="d-flex justify-content-between align-items-center px-3 mt-3 mb-3 mb-1 text-muted nav-item">
-							<span>
-								<i class="fas fa-archive"></i> 
-								<a href="{{ route('analisis.index') }}" class="nav-link d-inline p-0 {{ ! Route::is('analisis.*') ?: 'active' }}">
-									Análisis
-								</a>
-							</span>
-							<a class="d-flex align-items-center text-muted" href="{{ route('analisis.select') }}">
-								<i class="fas fa-plus-circle"></i>
-							</a>
-						</li>
-
-						<div class="dropdown-divider"></div>
-
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is(['procesos.*', 'indicadores.*', 'subindicadores.*']) ?: 'active' }}" href="{{ route('procesos.index') }}">
-								<i class="fas fa-arrows-alt-h"></i> 
-								Procesos
-							</a>
-						</li>
-
-						<li class="nav-item">
-							<a href="{{ route('estadisticas.index') }}" class="nav-link {{ ! Route::is('estadisticas.*') ?: 'active' }}">
-								<i class="fas fa-chart-area"></i>  
-								Estadísticas
-							</a>
-						</li>
-					</ul>
-
-					<div class="dropdown-divider"></div>
-
-					<ul class="nav flex-column mb-2">
-						<li class="nav-item">
-							<a class="nav-link {{ ! Route::is('users.*') ?: 'active' }}" href="{{ route('users.index') }}">
-								<i class="fas fa-users"></i> 
-								Usuarios
-							</a>
-						</li>
+						@include('fragments.menu')
 					</ul>
 				</div>
 			</nav>
