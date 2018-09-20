bb7b5d6e57bdd09e53f901412dbeab7a277a6d7a
Ivan Sauza <sauuza@gmail.com>
2018-09-20 01:35:29 -0500

    Permisos agregados

--- database/seeds/PermissionsTableSeeder.php
+++ database/seeds/PermissionsTableSeeder.php
@@ -1,6 +1,7 @@
 <?php
 
 use Illuminate\Database\Seeder;
+use Caffeinated\Shinobi\Models\Permission;
 
 class PermissionsTableSeeder extends Seeder
 {
@@ -11,6 +12,236 @@ class PermissionsTableSeeder extends Seeder
      */
     public function run()
     {
-        //
+        /**
+         * Modulo Periodos
+         */
+        Permission::create([
+            'name'        => 'Navegar periodos', 
+            'slug'        => 'periodos.index', 
+            'description' => 'Lista y navegar todos los periodos del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle del periodo', 
+            'slug'        => 'periodos.show', 
+            'description' => 'Ver en detalle cada periodo del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Creación de periodos', 
+            'slug'        => 'periodos.create', 
+            'description' => 'Crear periodos en el sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edición de periodos', 
+            'slug'        => 'periodos.edit', 
+            'description' => 'Editar cualquier dato de un periodo del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar periodos', 
+            'slug'        => 'periodos.destroy', 
+            'description' => 'Eliminar cualquier periodo del sistema.'
+        ]);
+
+        /**
+         * Modulo Preguntas
+         */
+        Permission::create([
+            'name'        => 'Navegar preguntas', 
+            'slug'        => 'preguntas.index', 
+            'description' => 'Lista y navegar todas las preguntas del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle de la pregunta', 
+            'slug'        => 'preguntas.show', 
+            'description' => 'Ver en detalle cada pregunta del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Creación de preguntas', 
+            'slug'        => 'preguntas.create', 
+            'description' => 'Crear preguntas en el sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edición de preguntas', 
+            'slug'        => 'preguntas.edit', 
+            'description' => 'Editar cualquier dato de un pregunta del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar preguntas', 
+            'slug'        => 'preguntas.destroy', 
+            'description' => 'Eliminar cualquier pregunta del sistema.'
+        ]);
+
+        /**
+         * Modulo Encuestas
+         */
+        Permission::create([
+            'name'        => 'Navegar encuestas', 
+            'slug'        => 'encuestas.index', 
+            'description' => 'Lista y navegar todas las encuestas del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle de la encuesta', 
+            'slug'        => 'encuestas.show', 
+            'description' => 'Ver en detalle cada encuesta del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Creación de encuestas', 
+            'slug'        => 'encuestas.create', 
+            'description' => 'Crear encuestas en el sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edición de encuestas', 
+            'slug'        => 'encuestas.edit', 
+            'description' => 'Editar cualquier dato de una encuesta del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar encuestas', 
+            'slug'        => 'encuestas.destroy', 
+            'description' => 'Eliminar cualquier encuesta del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Seleccionar usuario', 
+            'slug'        => 'encuestas.select.user', 
+            'description' => 'Seleccionar un usuario para crear una encuesta.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Seleccionar periodo', 
+            'slug'        => 'encuestas.select.periodo', 
+            'description' => 'Seleccionar un periodo para crear una encuesta.'
+        ]);
+
+        /**
+         * Modulo Análisis
+         */
+        /*Permission::create([
+            'name'        => 'Navegar análisis', 
+            'slug'        => 'analisis.index', 
+            'description' => 'Lista y navegar todos los análisis del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle del análisis', 
+            'slug'        => 'analisis.show', 
+            'description' => 'Ver en detalle cada análisis del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Creación de análisis', 
+            'slug'        => 'analisis.create', 
+            'description' => 'Crear análisis en el sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edición de análisis', 
+            'slug'        => 'analisis.edit', 
+            'description' => 'Editar cualquier dato de un análisis del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar análisis', 
+            'slug'        => 'analisis.destroy', 
+            'description' => 'Eliminar cualquier análisis del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Seleccionar periodo', 
+            'slug'        => 'analisis.select', 
+            'description' => 'Seleccionar un periodo para crear un análisis.'
+        ]);*/
+
+        /**
+         * Modulo Procesos
+         */
+        Permission::create([
+            'name'        => 'Navegar procesos', 
+            'slug'        => 'procesos.index', 
+            'description' => 'Lista y navegar todos los procesos del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle del proceso', 
+            'slug'        => 'procesos.show', 
+            'description' => 'Ver en detalle cada proceso del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Creación de procesos', 
+            'slug'        => 'procesos.create', 
+            'description' => 'Crear procesos en el sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edición de procesos', 
+            'slug'        => 'procesos.edit', 
+            'description' => 'Editar cualquier dato de un proceso del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar procesos', 
+            'slug'        => 'procesos.destroy', 
+            'description' => 'Eliminar cualquier proceso del sistema.'
+        ]);
+
+        /**
+         * Modulo Estadísticas
+         */
+        Permission::create([
+            'name'        => 'Navegar estadísticas', 
+            'slug'        => 'estadisticas.index', 
+            'description' => 'Lista y navegar todos los estadísticas del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle de la estadística', 
+            'slug'        => 'estadisticas.details', 
+            'description' => 'Ver en detalle cada estadística del sistema.'
+        ]);
+
+        /**
+         * Modulo Usuarios
+         */
+        Permission::create([
+            'name'        => 'Navegar users', 
+            'slug'        => 'users.index', 
+            'description' => 'Lista y navegar todos los users del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Ver detalle del user', 
+            'slug'        => 'users.show', 
+            'description' => 'Ver en detalle cada user del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Creación de users', 
+            'slug'        => 'users.create', 
+            'description' => 'Crear users en el sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Edición de users', 
+            'slug'        => 'users.edit', 
+            'description' => 'Editar cualquier dato de un user del sistema.'
+        ]);
+
+        Permission::create([
+            'name'        => 'Eliminar users', 
+            'slug'        => 'users.destroy', 
+            'description' => 'Eliminar cualquier user del sistema.'
+        ]);
+        
     }
 }
