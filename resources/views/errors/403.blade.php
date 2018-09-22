6ae9f11f76f77c2884dbe50a359728a0439532f7
Ivan Sauza <sauuza@gmail.com>
2018-09-22 16:01:23 -0500

    Plantilla del error 403 agregada

---
+++ resources/views/errors/403.blade.php
@@ -0,0 +1,6 @@
+@extends('layouts.admin')
+
+@section('content')
+    <h1 class="text-center h2">403 -  Acción no autorizada.</h1>
+    <a href="{{ route('home') }}" class="text-center">Ir al Inicio</a>
+@endsection
