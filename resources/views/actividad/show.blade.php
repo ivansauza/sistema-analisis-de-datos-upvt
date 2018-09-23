050ddaf26ad1727539cde180ec58691949f42ca8
Ivan Sauza <sauuza@gmail.com>
2018-09-23 15:38:45 -0500

    Mejoras en las vistas

--- resources/views/actividad/show.blade.php
+++ resources/views/actividad/show.blade.php
@@ -10,7 +10,34 @@
 
 @section('script')
 	$(document).ready( function () {
-		$('table').DataTable();
+		$( 'table' ).DataTable( {
+			"paging": false,
+			"aaSorting": [], 
+			"language": {
+				"sProcessing":     "Procesando...",
+				"sLengthMenu":     "Mostrar _MENU_ registros",
+				"sZeroRecords":    "No se encontraron resultados",
+				"sEmptyTable":     "Ningún dato disponible en esta tabla",
+				"sInfo":           "Mostrando registros del _START_ al _END_ de un total de _TOTAL_ registros",
+				"sInfoEmpty":      "Mostrando registros del 0 al 0 de un total de 0 registros",
+				"sInfoFiltered":   "(filtrado de un total de _MAX_ registros)",
+				"sInfoPostFix":    "",
+				"sSearch":         "Buscar:",
+				"sUrl":            "",
+				"sInfoThousands":  ",",
+				"sLoadingRecords": "Cargando...",
+				"oPaginate": {
+					"sFirst":    "Primero",
+					"sLast":     "Último",
+					"sNext":     "Siguiente",
+					"sPrevious": "Anterior"
+				},
+				"oAria": {
+					"sSortAscending":  ": Activar para ordenar la columna de manera ascendente",
+					"sSortDescending": ": Activar para ordenar la columna de manera descendente"
+				}
+			}
+		} )
 	} );
 @endsection
 
@@ -21,24 +48,30 @@
 @section('content')
 	<div class="card card-default">
 		<table class="table table-striped table-hover table-responsive-sm">
-			<thead>
+			<thead class="thead-light">
 				<tr>
 					<th scope="col">Acción</th>
 					<th scope="col">Url</th>
 					<th scope="col">Ip</th>
-					<th scope="col">Ubicación</th>
+					<th scope="col">Método</th>
 					<th scope="col">Fecha de registro</th>
 				</tr>
 			</thead>
 			<tbody>
-				<tr>
-					<td>login.success</td>
-					<td>Url</td>
-					<td>177.247.101.251</td>
-					<td>Mexico</td>
-					<td>Aug 31, 2018 00:16</td>
-				</tr>
+				@foreach ($actividades as $actividad)
+					<tr>
+						<td>{{ $actividad->ruta }}</td>
+						<td>{{ $actividad->url }}</td>
+						<td>{{ $actividad->ip }}</td>
+						<td>{{ $actividad->metodo }}</td>
+						<td>{{ $actividad->created_at }}</td>
+					</tr>
+				@endforeach
 			</tbody>
 		</table>
+
+		<span class="m-auto">
+		{{ $actividades->render("pagination::bootstrap-4") }}
+		</span>
 	</div>
 @endsection
