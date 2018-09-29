d45df8d14403b63cb1e285caceba9ab7b923d9c0
Ivan Sauza <sauuza@gmail.com>
2018-09-29 15:49:19 -0500

    Se agregó un estilo personalizado al titulo

--- resources/views/errors/403.blade.php
+++ resources/views/errors/403.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.admin')
 
+@section('style')
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif !important;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
+@endsection
+
 @section('content')
     <h1 class="text-center h2">403 -  Acción no autorizada.</h1>
     <br />
--- resources/views/errors/404.blade.php
+++ resources/views/errors/404.blade.php
@@ -1,5 +1,18 @@
 @extends('layouts.home')
 
+@section('style')
+    <style>
+        html, body {
+            background-color: #fff;
+            color: #636b6f;
+            font-family: 'Raleway', sans-serif !important;
+            font-weight: 100;
+            height: 100vh;
+            margin: 0;
+        }
+    </style>
+@endsection
+
 @section('content')
 	<div class="container">
 		<div class="row">
