faa6d3b6135c8204e6f3336d9a481af2d2c6dd22
unknown <sauuza@gmail.com>
2018-11-25 23:41:42 -0600

    Se agregarón mejoras a los reportes

--- resources/views/periodo/report.blade.php
+++ resources/views/periodo/report.blade.php
@@ -46,10 +46,61 @@
                                     <tr>
                                         <td class="border-right" style="width: 300px !important;">
                                             {{ $subindicador->nombre }}: 
-                                            <span class="text-muted">{{ $subindicador->getProcedimiento() }}</span> 
+                                            (<span class="text-muted">{{ $subindicador->getProcedimiento() }}</span>) 
                                         </td>
                                         <td class="text-center border-right">{{ $subindicador->valor_meta }}%</td>
-                                        <td class="border-right">{{ $subindicador->calculateProcedimiento($periodo->id) }}</td>
+                                        <td class="border-right text-center">
+                                            @foreach($subindicador->procedimiento as $item)
+                                                @switch ($item['type'])
+                                                    @case('pregunta')
+                                                        @php
+                                                            $pregunta  = App\Pregunta::find($item['value']);
+
+                                                            $encuestas = $periodo->encuestas()->whereHas('preguntas', function ($query) use ($pregunta) 
+                                                            {
+                                                                $query->where('pregunta_id', '=', $pregunta->id);
+
+                                                            })->WhereHas('periodo', function ($query) 
+                                                            {
+                                                                $query->whereIn('user_id', App\User::get()->pluck('id'));
+                                                            })->get();
+                                                        @endphp
+
+                                                        @php
+                                                            $total = 0;
+                                                        @endphp
+
+                                                        @foreach($encuestas as $encuesta)
+                                                            @php
+                                                                $total += $encuesta->getValuePregunta( $pregunta->id )
+                                                            @endphp
+                                                        @endforeach
+                                                        {{ $total }}
+                                                    @break
+
+                                                    @case('operacion')
+                                                        {!! "&frasl;" !!}
+                                                    @break
+                                                @endswitch
+                                            @endforeach
+
+                                             {{ auth()->user()->programas()->where('predeterminado', '=', 1)->first()->clave }} =
+
+                                            @php
+                                                $result = $subindicador->calculateProcedimiento($periodo->id)
+                                            @endphp
+
+                                            @if(is_numeric($result))
+                                                <a href="{{ route('estadisticas.details', ['subindicador' => $subindicador->id, 'periodo' => $periodo->id]) }}" class="text-default">
+
+                                                    {{ number_format((float)($result * 100), 1, '.', '') }}%
+                                                </a>
+                                            @else
+                                                <small>
+                                                    {{ $result }}
+                                                </small>
+                                            @endif
+                                        </td>
                                         <td class="border-right"></td>
                                         <td class="border-right"></td>
                                         <td style="width: 250px !important;">
